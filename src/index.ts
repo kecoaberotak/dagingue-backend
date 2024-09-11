8a953f9fa0457656d79c92f44b97792561c7354c
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-11 15:33:07 +0700

    Add: implement utility server on index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import express, { Application, Request, Response } from 'express';
+import express from 'express';
 import { logger } from './utils/logger';
 import CONFIG from './config/environtment';
+import createServer from './utils/server';
 
-const app: Application = express();
+const app = createServer();
 const port = CONFIG.port;
 
 app.use(express.json());
-
-app.get('/health', (req: Request, res: Response) => {
-  res.status(200).send({ status: true, statusCode: 200, message: 'Server is running' });
-});
 
 app.listen(port, () => {
   logger.info(`[server]: Server is running at port: ${port}`);
