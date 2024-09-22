851f92da0ba6263bb36bf17fd27fb7030c74a963
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-22 21:09:06 +0700

    Create: route for add potong with multer as middleware

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { logger } from '../utils/logger';
-import { getPotong } from '../controllers/potong.controller';
+import { upload } from '../middlewares/multer';
+import { addPotong, getPotong } from '../controllers/potong.controller';
 
 export const ProductRoute: Router = Router();
 
 ProductRoute.get('/potong', getPotong);
 ProductRoute.get('/potong/:id', getPotong);
+ProductRoute.post('/potong', upload.single('image'), addPotong);
 
 ProductRoute.get('/bumbu', (req, res) => {
   logger.info('Route check success');
