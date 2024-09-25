56b19ec47352cd41608b338c0253250c752bdd09
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-25 17:52:10 +0700

    Create: method potong validation for check data from user request

---
+++ src/validations/potong.validation.ts
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+import PotongType from '../types/potong.type';
+
+export const createPotongValidation = (payload: PotongType) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    desc: Joi.string().required(),
+    price: Joi.number().required(),
+    image: Joi.string().required().messages({
+      'any.required': 'Gambar harus diupload',
+    }),
+  });
+
+  return schema.validate(payload);
+};
