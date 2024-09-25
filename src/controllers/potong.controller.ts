7b656299461718a0904e7a716aeeccb1bd85e38b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-25 17:50:15 +0700

    Create: controller for handle request data potong from user

--- src/controllers/potong.controller.ts
+++ src/controllers/potong.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { logger } from '../utils/logger';
 import { addDataPotong, getAllDataPotong, getDataPotongById } from '../services/potong.service';
+import { createPotongValidation } from '../validations/potong.validation';
 
 export const getPotong = async (req: Request, res: Response) => {
   const {
@@ -25,21 +26,57 @@ export const getPotong = async (req: Request, res: Response) => {
 
 export const addPotong = async (req: Request, res: Response) => {
   const { name, desc, price } = req.body;
-  const image = req.file;
 
-  try {
-    // Kirim data form ke service untuk diproses
-    const newPotong = await addDataPotong({ name, desc, price, image });
+  if (req.file) {
+    const fileType = ['image/jpeg', 'image/jpg', 'image/png'];
 
-    if (newPotong) {
-      logger.info('Success add data potong');
-      return res.status(201).send({ status: true, statusCode: 201, data: newPotong });
-    } else {
-      logger.info('Failed to add data potong, missing or invalid fields');
-      return res.status(400).send({ status: false, statusCode: 400, message: 'Invalid input data or file type' });
+    // validasi tipe file
+    if (!fileType.includes(req.file.mimetype)) {
+      logger.info('Format file tidak valid');
+      return res.status(400).send({ message: 'Format file tidak valid' });
     }
-  } catch (error) {
-    logger.error('Error adding data potong', error);
-    return res.status(500).send({ status: false, statusCode: 500, message: 'Internal Server Error' });
+
+    // Jika file valid, lanjutkan ke proses berikutnya
+    const image = req.file;
+
+    // Validasi inputan user menggunakan Joi
+    const { error, value } = createPotongValidation({
+      name,
+      desc,
+      price,
+      image: image.originalname,
+    });
+
+    if (error) {
+      logger.error(`Err: potong - create = ${error.details[0].message}`);
+      return res.status(422).send({
+        status: false,
+        statusCode: 422,
+        message: error.details[0].message,
+        data: {},
+      });
+    }
+
+    // add to db
+    try {
+      const newPotong = await addDataPotong({ name: value.name, desc: value.desc, price: value.price, image });
+      if (newPotong) {
+        logger.info('Success add data potong');
+        return res.status(201).send({ status: true, statusCode: 201, data: newPotong });
+      } else {
+        logger.info('Failed to add data potong, missing or invalid fields');
+        return res.status(400).send({ status: false, statusCode: 400, message: 'Invalid input data or file type' });
+      }
+    } catch (error) {
+      logger.error(`Err: potong - create = ${error}`);
+      return res.status(422).send({
+        status: false,
+        statusCode: 422,
+        message: error,
+      });
+    }
+  } else {
+    logger.info('Missing image file');
+    return res.status(400).send({ message: 'Missing image file' });
   }
 };
