924e2cd3a4be9a467f1673a6f084350e511e6ecd
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-26 19:58:53 +0700

    Add: comment

--- src/validations/potong.validation.ts
+++ src/validations/potong.validation.ts
@@ -1,11 +1,12 @@
 import Joi from 'joi';
-import PotongType from '../types/potong.type';
+import { PotongType } from '../types/potong.type';
 
 export const createPotongValidation = (payload: PotongType) => {
   const schema = Joi.object({
     name: Joi.string().required(),
     desc: Joi.string().required(),
     price: Joi.number().required(),
+    // Joi.alternatives() ??
     image: Joi.string().required().messages({
       'any.required': 'Gambar harus diupload',
     }),
