bcc64a8a23bfdd3236fdd514a4d475cfa51359ad
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-26 19:59:56 +0700

    Edit: refactoring code

--- src/services/potong.service.ts
+++ src/services/potong.service.ts
@@ -1,16 +1,17 @@
 import { db } from '../lib/firebase/init';
+import { PotongType, UploadPotongResult } from '../types/potong.type';
 import { logger } from '../utils/logger';
 import { uploadImageToStorage } from '../utils/uploadImageToStorage';
 
-export const getAllDataPotong = async () => {
+export const getAllDataPotong = async (): Promise<UploadPotongResult> => {
   try {
     // ambil semua data
     const snapshot = await db.collection('potongs').get();
 
     // Mengecek apakah ada data
     if (snapshot.empty) {
-      logger.info('No match document');
-      return null;
+      logger.info('No potong data found');
+      return { success: false, message: 'No potong data found' };
     }
 
     // Membuat array untuk menampung data
@@ -19,60 +20,82 @@ export const getAllDataPotong = async () => {
       ...doc.data(),
     }));
 
-    return data;
+    return {
+      success: true,
+      message: 'Success upload new data potong to Database',
+      data: data,
+    };
   } catch (error) {
-    logger.info('Error getting data potong');
-    logger.error('Error getting data potong:', error);
-    return null;
+    if (error instanceof Error) {
+      logger.error(`Err: potong - get all data = ${error.message}`);
+      return { success: false, message: error.message };
+    } else {
+      logger.error('Err: potong - get all data = Unknown error');
+      return { success: false, message: 'Unknown error occurred' };
+    }
   }
 };
 
-export const getDataPotongById = async (id: string) => {
+export const getDataPotongById = async (id: string): Promise<UploadPotongResult> => {
   try {
     const snapshot = await db.collection('potongs').doc(id).get();
 
     if (!snapshot.exists) {
-      logger.info('No match document');
-      return null;
+      logger.info('No potong data found for ID: ' + id);
+      return { success: false, message: 'No potong data found for ID: ' + id };
     }
 
     return {
-      id: snapshot.id,
-      ...snapshot.data(),
+      success: true,
+      message: 'Success upload new data potong to Database',
+      data: { id: snapshot.id, ...snapshot.data() },
     };
   } catch (error) {
-    logger.info('Error getting data potong');
-    logger.error('Error getting data potong:', error);
-    return null;
+    if (error instanceof Error) {
+      logger.error(`Err: potong - get data by id = ${error.message}`);
+      return { success: false, message: error.message };
+    } else {
+      logger.error('Err: potong - get data by id = Unknown error');
+      return { success: false, message: 'Unknown error occurred' };
+    }
   }
 };
 
-export const addDataPotong = async (payload: any) => {
+export const addDataPotong = async (payload: PotongType): Promise<UploadPotongResult> => {
   const { name, desc, price, image } = payload;
 
   try {
     // Upload gambar ke Cloud Storage
-    const imageUrl = await uploadImageToStorage(image);
+    const result = await uploadImageToStorage(image as Express.Multer.File);
 
-    if (!imageUrl) {
-      logger.info('Failed upload image to storage');
-      return null;
+    if (!result.success) {
+      logger.info(`Upload image failed: ${result.message}`);
+      return { success: false, message: 'Upload image failed to Storage' };
     }
 
     // Simpan data ke Firestore
     const newData = {
       name,
       desc,
-      price: parseFloat(price), // Konversi harga menjadi angka jika dikirim dalam string
-      image: imageUrl, // URL gambar dari Cloud Storage
+      price, // Konversi harga menjadi angka jika dikirim dalam string
+      image: result.imageLink, // URL gambar dari Cloud Storage
       createdAt: new Date(), // Tambahkan timestamp
     };
 
     // Simpan ke dalam koleksi 'potongs'
     const potongRef = await db.collection('potongs').add(newData);
-    return { id: potongRef.id, ...newData };
+    return {
+      success: true,
+      message: 'Success upload new data potong to Database',
+      data: { id: potongRef.id, ...newData },
+    };
   } catch (error) {
-    logger.error(`Err: potong - add to database = ${error}`);
-    return null;
+    if (error instanceof Error) {
+      logger.error(`Err: potong - add data to database = ${error.message}`);
+      return { success: false, message: error.message };
+    } else {
+      logger.error('Err: potong - add data to database = Unknown error');
+      return { success: false, message: 'Unknown error occurred' };
+    }
   }
 };
