4c3bf595ff56d4e562a5f9ec7a7ce5e98d1a644d
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-26 20:00:21 +0700

    Edit: refactoring code controller

--- src/controllers/potong.controller.ts
+++ src/controllers/potong.controller.ts
@@ -9,17 +9,17 @@ export const getPotong = async (req: Request, res: Response) => {
   } = req;
 
   try {
-    const data = id ? await getDataPotongById(id) : await getAllDataPotong();
+    const result = id ? await getDataPotongById(id) : await getAllDataPotong();
 
-    if (data) {
+    if (result.success) {
       logger.info(`Success get data ${id ? 'detail ' : ''}potong`);
-      return res.status(200).send({ status: true, statusCode: 200, data });
+      return res.status(200).send({ status: true, statusCode: 200, data: result.data });
     } else {
       logger.info('Data not found');
-      return res.status(404).send({ status: false, statusCode: 404, message: 'Data not found' });
+      return res.status(404).send({ status: false, statusCode: 404, message: result.message });
     }
   } catch (error) {
-    logger.error('Error fetching data potong', error);
+    logger.error(`Err: potong - get = ${error}`);
     return res.status(500).send({ status: false, statusCode: 500, message: 'Internal Server Error' });
   }
 };
@@ -27,56 +27,46 @@ export const getPotong = async (req: Request, res: Response) => {
 export const addPotong = async (req: Request, res: Response) => {
   const { name, desc, price } = req.body;
 
-  if (req.file) {
-    const fileType = ['image/jpeg', 'image/jpg', 'image/png'];
-
-    // validasi tipe file
-    if (!fileType.includes(req.file.mimetype)) {
-      logger.info('Format file tidak valid');
-      return res.status(400).send({ message: 'Format file tidak valid' });
-    }
-
-    // Jika file valid, lanjutkan ke proses berikutnya
-    const image = req.file;
-
-    // Validasi inputan user menggunakan Joi
-    const { error, value } = createPotongValidation({
-      name,
-      desc,
-      price,
-      image: image.originalname,
-    });
-
-    if (error) {
-      logger.error(`Err: potong - create = ${error.details[0].message}`);
-      return res.status(422).send({
-        status: false,
-        statusCode: 422,
-        message: error.details[0].message,
-        data: {},
-      });
-    }
-
-    // add to db
-    try {
-      const newPotong = await addDataPotong({ name: value.name, desc: value.desc, price: value.price, image });
-      if (newPotong) {
-        logger.info('Success add data potong');
-        return res.status(201).send({ status: true, statusCode: 201, data: newPotong });
-      } else {
-        logger.info('Failed to add data potong, missing or invalid fields');
-        return res.status(400).send({ status: false, statusCode: 400, message: 'Invalid input data or file type' });
-      }
-    } catch (error) {
-      logger.error(`Err: potong - create = ${error}`);
-      return res.status(422).send({
-        status: false,
-        statusCode: 422,
-        message: error,
-      });
-    }
-  } else {
+  if (!req.file) {
     logger.info('Missing image file');
     return res.status(400).send({ message: 'Missing image file' });
+  }
+
+  // Validasi inputan user menggunakan Joi
+  const { error, value } = createPotongValidation({
+    name,
+    desc,
+    price,
+    image: req.file,
+  });
+
+  if (error) {
+    logger.error(`Err: potong - create = ${error.details[0].message}`);
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error.details[0].message,
+      data: {},
+    });
+  }
+
+  // add to db
+  try {
+    const result = await addDataPotong({ name: value.name, desc: value.desc, price: value.price, image: req.file });
+    if (result.success) {
+      logger.info('Success add data potong');
+      return res.status(201).send({ status: true, statusCode: 201, message: result.message, data: result.data });
+    } else {
+      logger.info(result.message);
+      return res.status(400).send({ status: false, statusCode: 400, message: result.message });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(`Err: potong - create data potong = ${error.message}`);
+      return res.status(500).send({ status: false, statusCode: 500, message: error.message });
+    } else {
+      logger.error('Err: potong - create data potong = Unknown error');
+      return res.status(500).send({ status: false, statusCode: 500, message: 'Unknown error occurred' });
+    }
   }
 };
