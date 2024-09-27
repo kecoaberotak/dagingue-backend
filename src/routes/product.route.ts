398da2eca8ad6ac99035b1d2ba9d491646721905
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-27 14:56:31 +0700

    Add: route for getBumbu

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { logInfo } from '../utils/logger';
 import { upload } from '../middlewares/multer';
 import { addPotong, getPotong } from '../controllers/potong.controller';
+import { getBumbu } from '../controllers/bumbu.controller';
 
 export const ProductRoute: Router = Router();
 
@@ -9,7 +9,4 @@ ProductRoute.get('/potong', getPotong);
 ProductRoute.get('/potong/:id', getPotong);
 ProductRoute.post('/potong', upload.single('image'), addPotong);
 
-ProductRoute.get('/bumbu', (req, res) => {
-  logInfo('Route check success');
-  res.status(200).send({ message: 'Product Bumbu' });
-});
+ProductRoute.get('/bumbu', getBumbu);
