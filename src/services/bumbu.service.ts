24f73979bc79e6a529343b8f3e4da16416ab3271
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-27 15:17:29 +0700

    Create: service to upload new data bumbu

--- src/services/bumbu.service.ts
+++ src/services/bumbu.service.ts
@@ -1,5 +1,6 @@
 import { db } from '../lib/firebase/init';
 import { ProductType, ProductResultType } from '../types/product.type';
+import { uploadImageToStorage } from '../utils/uploadImageToStorage';
 
 export const getAllDataBumbu = async (): Promise<ProductResultType> => {
   try {
@@ -29,15 +30,46 @@ export const getDataBumbuById = async (id: string): Promise<ProductResultType> =
     const snapshot = await db.collection('bumbus').doc(id).get();
 
     if (!snapshot.exists) {
-      return { success: false, message: 'No data potong found for ID: ' + id };
+      return { success: false, message: 'No data bumbu found for ID: ' + id };
     }
 
     return {
       success: true,
-      message: 'Success get data potong for ID: ' + id,
+      message: 'Success get data bumbu for ID: ' + id,
       data: { id: snapshot.id, ...snapshot.data() },
     };
   } catch (error) {
     throw error;
+  }
+};
+
+export const addDataBumbu = async (payload: ProductType): Promise<ProductResultType> => {
+  const { name, desc, price, image } = payload;
+
+  try {
+    const imageLink = uploadImageToStorage(image as Express.Multer.File, 'bumbu_image');
+
+    const newData = {
+      name,
+      desc,
+      price,
+      image: imageLink,
+      createdAt: new Date(), // Tambahkan timestamp
+    };
+    const bumbuRef = await db.collection('bumbus').add(newData);
+    return {
+      success: true,
+      message: 'Success upload new data bumbu to Database',
+      data: { id: bumbuRef.id, ...newData },
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        success: false,
+        message: `Error occurred while add new data potong: ${error.message}`,
+      };
+    } else {
+      return { success: false, message: 'Unknown error occurred while add new data potong' };
+    }
   }
 };
