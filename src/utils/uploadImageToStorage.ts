2d87d076b2b9c1071a30af8d484b3ec873a2ef37
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-27 15:56:19 +0700

    Edit: parameter name

--- src/utils/uploadImageToStorage.ts
+++ src/utils/uploadImageToStorage.ts
@@ -1,13 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 import { storage } from '../lib/firebase/init';
 
-export const uploadImageToStorage = async (file: Express.Multer.File, bucket: string): Promise<string> => {
+export const uploadImageToStorage = async (file: Express.Multer.File, storageName: string): Promise<string> => {
   try {
     // Generate Unique file name
     const fileExtension = file.mimetype.split('/')[1];
     const filename = `${uuidv4()}.${fileExtension}`;
     const bucket = storage.bucket();
-    const fileRef = bucket.file(`${bucket}/${filename}`);
+    const fileRef = bucket.file(`${storageName}/${filename}`);
 
     // Upload file ke Cloud Storage
     await fileRef.save(file.buffer, {
