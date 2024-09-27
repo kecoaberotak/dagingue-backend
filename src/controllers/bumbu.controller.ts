3eed6a0362ce4bb9e0cb11cd5eb05097c59791ee
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-27 19:30:02 +0700

    Create: controller for delete bumbu

--- src/controllers/bumbu.controller.ts
+++ src/controllers/bumbu.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { logError, logInfo } from '../utils/logger';
-import { addDataBumbu, getAllDataBumbu, getDataBumbuById } from '../services/bumbu.service';
+import { addDataBumbu, deleteDataBumbuById, getAllDataBumbu, getDataBumbuById } from '../services/bumbu.service';
 import { ResponseDataType } from '../types/product.type';
 import { createProductValidation } from '../validations/product.validation';
 
@@ -22,7 +22,7 @@ export const getBumbu = async (req: Request, res: Response) => {
       };
       return res.status(200).send(response);
     } else {
-      logError(`Failed to ${id ? 'get data bumbu by id' : 'get all data bumbu'}: ${result.message}`);
+      logError(result.message);
       const response: ResponseDataType = { status: false, statusCode: 404, message: result.message, data: {} };
       return res.status(404).send(response);
     }
@@ -104,6 +104,44 @@ export const addBumbu = async (req: Request, res: Response) => {
         statusCode: 500,
         message: 'Unknown error occurred',
         data: {},
+      };
+      return res.status(500).send(response);
+    }
+  }
+};
+
+export const deleteBumbu = async (req: Request, res: Response) => {
+  const {
+    params: { id },
+  } = req;
+
+  try {
+    const result = await deleteDataBumbuById(id);
+
+    if (result.success) {
+      logInfo(result.message);
+      const response: ResponseDataType = {
+        status: true,
+        statusCode: 200,
+        message: result.message,
+      };
+      return res.status(200).send(response);
+    } else {
+      logError(result.message);
+      const response: ResponseDataType = { status: false, statusCode: 404, message: result.message };
+      return res.status(404).send(response);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      logError('Error occurred while executing deleteDataBumbuById', error);
+      const response: ResponseDataType = { status: false, statusCode: 500, message: error.message };
+      return res.status(500).send(response);
+    } else {
+      logError('Unknown error occurred while executing deleteDataBumbuById');
+      const response: ResponseDataType = {
+        status: false,
+        statusCode: 500,
+        message: 'Unknown error occurred',
       };
       return res.status(500).send(response);
     }
