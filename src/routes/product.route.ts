bc5a6c967a5be79f1f2c036d0d723fcfb7420555
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-27 18:03:36 +0700

    Create: endpoint for delete data potong

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { upload } from '../middlewares/multer';
-import { addPotong, getPotong } from '../controllers/potong.controller';
+import { addPotong, deletePotong, getPotong } from '../controllers/potong.controller';
 import { addBumbu, getBumbu } from '../controllers/bumbu.controller';
 
 export const ProductRoute: Router = Router();
@@ -8,6 +8,7 @@ export const ProductRoute: Router = Router();
 ProductRoute.get('/potong', getPotong);
 ProductRoute.get('/potong/:id', getPotong);
 ProductRoute.post('/potong', upload.single('image'), addPotong);
+ProductRoute.delete('/potong/:id', deletePotong);
 
 ProductRoute.get('/bumbu', getBumbu);
 ProductRoute.get('/bumbu/:id', getBumbu);
