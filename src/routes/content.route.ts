548b78d690e497a1b3869223f2aed7465e6cce87
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-29 14:35:58 +0700

    Create: route for add data about

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { logInfo } from '../utils/logger';
+import { upload } from '../middlewares/multer';
+import { addAbout } from '../controllers/about.controller';
 
 export const ContentRoute: Router = Router();
 
-ContentRoute.get('/', (req, res) => {
-  logInfo('Route check success');
-  res.status(200).send({ message: 'Content' });
-});
+ContentRoute.post(
+  '/about',
+  upload.fields([
+    { name: 'image1', maxCount: 1 },
+    { name: 'image2', maxCount: 1 },
+  ]),
+  addAbout,
+);
