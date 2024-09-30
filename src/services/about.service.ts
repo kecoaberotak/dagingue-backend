6731cfbf5191cbc2887888c5001e5684d233dda4
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-30 18:39:18 +0700

    Create: service for delete data content about

--- src/services/about.service.ts
+++ src/services/about.service.ts
@@ -1,6 +1,7 @@
 import { db } from '../lib/firebase/init';
 import { uploadImageToStorage } from '../utils/uploadImageToStorage';
 import { ContentType, ContentResultType } from '../types/content.types';
+import { deleteImageFromStorage } from '../utils/deleteImageFromStorage';
 
 export const addDataAbout = async (payload: ContentType): Promise<ContentResultType> => {
   const { desc, image1, image2 } = payload;
@@ -72,5 +73,60 @@ export const getDataAboutById = async (id: string): Promise<ContentResultType> =
     };
   } catch (error) {
     throw error;
+  }
+};
+
+export const deleteDataAboutById = async (id: string): Promise<ContentResultType> => {
+  try {
+    const aboutRef = db.collection('abouts').doc(id);
+    const snapshot = await aboutRef.get();
+
+    if (!snapshot.exists) {
+      return { success: false, message: 'No data content about found for ID: ' + id };
+    }
+
+    const data = snapshot.data();
+
+    const imageUrl1 = data?.image1;
+    if (imageUrl1) {
+      try {
+        await deleteImageFromStorage(imageUrl1);
+      } catch (error) {
+        if (error instanceof Error) {
+          return {
+            success: false,
+            message: `Failed to delete image for content about with ID ${id}: ${error.message}`,
+          };
+        }
+        return { success: false, message: 'Unknown error occurred during deletion' };
+      }
+    } else if (!imageUrl1) {
+      return { success: false, message: 'No image link found for ID: ' + id };
+    }
+
+    const imageUrl2 = data?.image2;
+    if (imageUrl2) {
+      try {
+        await deleteImageFromStorage(imageUrl2);
+      } catch (error) {
+        if (error instanceof Error) {
+          return {
+            success: false,
+            message: `Failed to delete image for content about with ID ${id}: ${error.message}`,
+          };
+        }
+        return { success: false, message: 'Unknown error occurred during deletion' };
+      }
+    } else if (!imageUrl2) {
+      return { success: false, message: 'No image link found for ID: ' + id };
+    }
+
+    await aboutRef.delete();
+    return { success: true, message: `Success delete data content about for ID: ${id}` };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { success: false, message: `Failed to delete image for content about with ID ${id}: ${error.message}` };
+    }
+    return { success: false, message: 'Unknown error occurred during deletion' };
   }
 };
