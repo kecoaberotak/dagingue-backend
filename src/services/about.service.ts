2bdf4175579553adb21ee078a45f2abd86612c83
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-30 19:57:32 +0700

    Create: service for update data about

--- src/services/about.service.ts
+++ src/services/about.service.ts
@@ -1,9 +1,9 @@
 import { db } from '../lib/firebase/init';
 import { uploadImageToStorage } from '../utils/uploadImageToStorage';
-import { ContentType, ContentResultType } from '../types/content.types';
+import { AboutType, ContentResultType } from '../types/content.types';
 import { deleteImageFromStorage } from '../utils/deleteImageFromStorage';
 
-export const addDataAbout = async (payload: ContentType): Promise<ContentResultType> => {
+export const addDataAbout = async (payload: AboutType): Promise<ContentResultType> => {
   const { desc, image1, image2 } = payload;
 
   try {
@@ -128,5 +128,60 @@ export const deleteDataAboutById = async (id: string): Promise<ContentResultType
       return { success: false, message: `Failed to delete image for content about with ID ${id}: ${error.message}` };
     }
     return { success: false, message: 'Unknown error occurred during deletion' };
+  }
+};
+
+export const updateDataAboutById = async (id: string, payload: AboutType): Promise<ContentResultType> => {
+  try {
+    const aboutRef = db.collection('abouts').doc(id);
+    const snapshot = await aboutRef.get();
+
+    if (!snapshot.exists) {
+      return { success: false, message: 'No potong data about for ID: ' + id };
+    }
+
+    let updatedImageLink1: string | undefined = payload.image1 as string;
+
+    if (payload.image1 && typeof payload.image1 !== 'string') {
+      // Jika image adalah file, upload ke Firebase Storage
+      updatedImageLink1 = await uploadImageToStorage(payload.image1 as Express.Multer.File, 'about_image');
+      try {
+        await deleteImageFromStorage(snapshot.data()?.image1);
+      } catch (error) {
+        if (error instanceof Error) {
+          return { success: false, message: `Failed to delete image for about with ID ${id}: ${error.message}` };
+        }
+        return { success: false, message: 'Unknown error occurred during deletion' };
+      }
+    }
+
+    let updatedImageLink2: string | undefined = payload.image2 as string;
+    if (payload.image2 && typeof payload.image2 !== 'string') {
+      updatedImageLink2 = await uploadImageToStorage(payload.image2 as Express.Multer.File, 'about_image');
+      try {
+        await deleteImageFromStorage(snapshot.data()?.image2);
+      } catch (error) {
+        if (error instanceof Error) {
+          return { success: false, message: `Failed to delete image for about with ID ${id}: ${error.message}` };
+        }
+        return { success: false, message: 'Unknown error occurred during deletion' };
+      }
+    }
+
+    const updatedData = {
+      desc: payload.desc ?? snapshot.data()?.desc,
+      image1: updatedImageLink1 ?? snapshot.data()?.image1,
+      image2: updatedImageLink2 ?? snapshot.data()?.image2,
+      updatedAt: new Date(),
+    };
+
+    await aboutRef.update(updatedData);
+    return {
+      success: true,
+      message: 'Success update data about for ID: ' + id,
+      data: { id: snapshot.id, ...updatedData },
+    };
+  } catch (error) {
+    throw error;
   }
 };
