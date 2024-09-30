e8b89e18a345b4ec21f5eb8b33c89c3446d8cae1
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-30 19:55:27 +0700

    Create: validation for update about

--- src/validations/content.validation.ts
+++ src/validations/content.validation.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
-import { ContentType } from '../types/content.types';
+import { AboutType } from '../types/content.types';
 
-export const createContentValidation = (payload: ContentType) => {
+export const createContentValidation = (payload: AboutType) => {
   const schema = Joi.object({
     desc: Joi.string().required().messages({
       'any.required': 'Deskripsi tidak boleh kosong atau data tidak valid',
@@ -24,6 +24,42 @@ export const createContentValidation = (payload: ContentType) => {
       .unknown(true)
       .required()
       .messages({ 'object.base': 'File image tidak valid', 'any.required': 'File gambar wajib diunggah' }),
+  });
+
+  return schema.validate(payload);
+};
+
+export const updateContentValidation = (payload: AboutType) => {
+  const schema = Joi.object({
+    desc: Joi.string().optional().messages({
+      'string.base': 'Deskripsi  tidak valid',
+    }),
+    image1: Joi.alternatives()
+      .try(
+        Joi.string().uri().optional(),
+        Joi.object({
+          mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/gif').required().messages({
+            'any.only': 'File harus berupa gambar dengan format jpeg, png, atau gif',
+          }),
+        })
+          .unknown(true)
+          .optional()
+          .messages({ 'object.base': 'File image tidak valid' }),
+      )
+      .optional(),
+    image2: Joi.alternatives()
+      .try(
+        Joi.string().uri().optional(),
+        Joi.object({
+          mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/gif').required().messages({
+            'any.only': 'File harus berupa gambar dengan format jpeg, png, atau gif',
+          }),
+        })
+          .unknown(true)
+          .optional()
+          .messages({ 'object.base': 'File image tidak valid' }),
+      )
+      .optional(),
   });
 
   return schema.validate(payload);
