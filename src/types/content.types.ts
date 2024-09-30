3bec1fddd753a9e8b699c03676cd79b9644a19a6
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-09-30 11:12:03 +0700

    Create: interface for result data type

--- src/types/content.types.ts
+++ src/types/content.types.ts
@@ -3,3 +3,9 @@ export interface ContentType {
   image1: Express.Multer.File | string;
   image2: Express.Multer.File | string;
 }
+
+export interface ContentResultType {
+  success: boolean;
+  message: string;
+  data?: { id?: string; [key: string]: any } | Array<{ id: string; [key: string]: any }>;
+}
