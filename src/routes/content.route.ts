9ca8ccc2d143413c7ba00acdd42f0332b21ea0fa
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-01 12:17:06 +0700

    Create: router for add data media

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,20 +1,21 @@
 import { Router } from 'express';
 import { upload } from '../middlewares/multer';
 import { addAbout, deleteAbout, getAbout, updateAbout } from '../controllers/about.controller';
+import { addMedia } from '../controllers/media.controller';
 
 export const ContentRoute: Router = Router();
 
-ContentRoute.post(
-  '/about',
-  upload.fields([
-    { name: 'image1', maxCount: 1 },
-    { name: 'image2', maxCount: 1 },
-  ]),
-  addAbout,
-);
+// ContentRoute.post(
+//   '/about',
+//   upload.fields([
+//     { name: 'image1', maxCount: 1 },
+//     { name: 'image2', maxCount: 1 },
+//   ]),
+//   addAbout,
+// );
 ContentRoute.get('/about', getAbout);
 ContentRoute.get('/about/:id', getAbout);
-ContentRoute.delete('/about/:id', deleteAbout);
+// ContentRoute.delete('/about/:id', deleteAbout);
 ContentRoute.put(
   '/about/:id',
   upload.fields([
@@ -22,4 +23,15 @@ ContentRoute.put(
     { name: 'image2', maxCount: 1 },
   ]),
   updateAbout,
+);
+
+ContentRoute.post(
+  '/media',
+  upload.fields([
+    { name: 'logo_image', maxCount: 1 },
+    { name: 'hero_image', maxCount: 1 },
+    { name: 'background_image', maxCount: 1 },
+    { name: 'footer_image', maxCount: 1 },
+  ]),
+  addMedia,
 );
