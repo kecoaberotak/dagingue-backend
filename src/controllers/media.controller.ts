eb772584e2ae3efa10e32ec0b7d49ae8630b82e1
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-01 14:19:24 +0700

    Add: implementing validation in controller addMedia

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { logInfo, logError } from '../utils/logger';
 import { ResponseDataType } from '../types/general.types';
+import { createMediaValidation } from '../validations/content.validation';
 
 export const addMedia = async (req: Request, res: Response) => {
   const { email, phone, address, instagram, shopee, whatsapp, maps } = req.body;
@@ -28,6 +29,32 @@ export const addMedia = async (req: Request, res: Response) => {
     return res.status(400).send(response);
   }
 
+  const { error, value } = createMediaValidation({
+    email,
+    phone,
+    address,
+    instagram,
+    shopee,
+    whatsapp,
+    maps,
+    logo_image: files.logo_image[0],
+    hero_image: files.hero_image[0],
+    background_image: files.background_image[0],
+    footer_image: files.footer_image[0],
+  });
+
+  if (error) {
+    logError(`Validation error while creating data: ${error.details[0].message}`);
+    const response: ResponseDataType = {
+      status: false,
+      statusCode: 422,
+      message: error.details[0].message,
+      data: {},
+    };
+    return res.status(422).send(response);
+  }
+
+  // test aja
   logInfo('Success add data media');
   const response: ResponseDataType = {
     status: true,
