dcd0f89cd7f6a529e3ca7abb4b3f0722b7afb456
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-01 12:17:20 +0700

    Create: service for add data media

---
+++ src/services/media.service.ts
@@ -0,0 +1,57 @@
+import { db } from '../lib/firebase/init';
+import { ContentResultType, MediaType } from '../types/content.types';
+import { uploadImageToStorage } from '../utils/uploadImageToStorage';
+
+export const addDataMedia = async (payload: MediaType): Promise<ContentResultType> => {
+  const {
+    email,
+    phone,
+    address,
+    logo_image,
+    hero_image,
+    background_image,
+    footer_image,
+    instagram,
+    shopee,
+    whatsapp,
+    maps,
+  } = payload;
+
+  try {
+    const logoImageLink = await uploadImageToStorage(logo_image as Express.Multer.File, 'media_image');
+    const heroImageLink = await uploadImageToStorage(hero_image as Express.Multer.File, 'media_image');
+    const backgroundImageLink = await uploadImageToStorage(background_image as Express.Multer.File, 'media_image');
+    const footerImageLink = await uploadImageToStorage(footer_image as Express.Multer.File, 'media_image');
+
+    const newData = {
+      email,
+      phone,
+      address,
+      instagram,
+      shopee,
+      whatsapp,
+      maps,
+      logo_image: logoImageLink,
+      hero_image: heroImageLink,
+      background_image: backgroundImageLink,
+      footer_image: footerImageLink,
+      createdAt: new Date(),
+    };
+
+    const mediaRef = await db.collection('medias').add(newData);
+    return {
+      success: true,
+      message: 'Success upload new data media to Database',
+      data: { id: mediaRef.id, ...newData },
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        success: false,
+        message: `Error occurred while add new data media about: ${error.message}`,
+      };
+    } else {
+      return { success: false, message: 'Unknown error occurred while add new data media' };
+    }
+  }
+};
