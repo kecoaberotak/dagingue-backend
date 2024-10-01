f923f90487bdb5c0b506c7214b0f2aedf2b39dd6
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-01 11:36:05 +0700

    Create: type interface for media content type

--- src/types/content.types.ts
+++ src/types/content.types.ts
@@ -4,6 +4,20 @@ export interface AboutType {
   image2: Express.Multer.File | string;
 }
 
+export interface MediaType {
+  email: string;
+  phone: string;
+  address: string;
+  maps: string;
+  instagram: string;
+  whatsapp: string;
+  shopee: string;
+  logo_image: Express.Multer.File | string;
+  hero_image: Express.Multer.File | string;
+  background_image: Express.Multer.File | string;
+  footer_image: Express.Multer.File | string;
+}
+
 export interface ContentResultType {
   success: boolean;
   message: string;
