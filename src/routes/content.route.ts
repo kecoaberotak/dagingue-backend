e5972d8219aec418c90f8e2dff7d383f438f8114
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-02 14:26:34 +0700

    Delete: Unuses routes

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,22 +1,13 @@
 import { Router } from 'express';
 import { upload } from '../middlewares/multer';
-import { addAbout, deleteAbout, getAbout, updateAbout } from '../controllers/about.controller';
-import { addMedia, getMedia, updateMedia } from '../controllers/media.controller';
+import { getAbout, updateAbout } from '../controllers/about.controller';
+import { getMedia, updateMedia } from '../controllers/media.controller';
 
 export const ContentRoute: Router = Router();
 
 // ABOUT
-// ContentRoute.post(
-//   '/about',
-//   upload.fields([
-//     { name: 'image1', maxCount: 1 },
-//     { name: 'image2', maxCount: 1 },
-//   ]),
-//   addAbout,
-// );
 ContentRoute.get('/about', getAbout);
 ContentRoute.get('/about/:id', getAbout);
-// ContentRoute.delete('/about/:id', deleteAbout);
 ContentRoute.put(
   '/about/:id',
   upload.fields([
@@ -27,16 +18,6 @@ ContentRoute.put(
 );
 
 // MEDIA
-// ContentRoute.post(
-//   '/media',
-//   upload.fields([
-//     { name: 'logo_image', maxCount: 1 },
-//     { name: 'hero_image', maxCount: 1 },
-//     { name: 'background_image', maxCount: 1 },
-//     { name: 'footer_image', maxCount: 1 },
-//   ]),
-//   addMedia,
-// );
 ContentRoute.get('/media', getMedia);
 ContentRoute.put(
   '/media',
