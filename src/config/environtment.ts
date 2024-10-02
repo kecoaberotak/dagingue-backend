ac82cde7d85060a32dcd67594840f17fdbf8f259
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-02 21:51:39 +0700

    Add: config for firebase api key

--- src/config/environtment.ts
+++ src/config/environtment.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 const CONFIG = {
   port: process.env.PORT,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.FIREBASE_API_KEY,
 };
 
 export default CONFIG;
