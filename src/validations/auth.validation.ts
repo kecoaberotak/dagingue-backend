6c5704ea344cb55d50767474f5f89b0b9f4689e9
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-03 14:53:50 +0700

    Create: validation for login

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,11 +1,24 @@
 import Joi from 'joi';
-import { RegisterTypes } from '../types/auth.types';
+import { LoginTypes, RegisterTypes } from '../types/auth.types';
 
 export const registerValidation = (payload: RegisterTypes) => {
   const schema = Joi.object({
     name: Joi.string().required().messages({
       'any.required': 'Nama tidak boleh kosong atau data tidak valid',
     }),
+    email: Joi.string().required().messages({
+      'any.required': 'Email tidak boleh kosong atau data tidak valid',
+    }),
+    password: Joi.string().required().messages({
+      'any.required': 'Password tidak boleh kosong atau data tidak valid',
+    }),
+  });
+
+  return schema.validate(payload);
+};
+
+export const loginValidation = (payload: LoginTypes) => {
+  const schema = Joi.object({
     email: Joi.string().required().messages({
       'any.required': 'Email tidak boleh kosong atau data tidak valid',
     }),
