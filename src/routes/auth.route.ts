dce21355500291a4344a38dd7141145f35bd64e2
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-04 18:24:48 +0700

    Create: auth for delete user

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import verifyToken from '../middlewares/verifyToken';
-import { login, registerAdmin } from '../controllers/auth.controller';
+import { deleteUser, login, registerAdmin } from '../controllers/auth.controller';
 
 export const AuthRoute: Router = Router();
 
 AuthRoute.post('/login', login);
 AuthRoute.post('/register', verifyToken, registerAdmin);
+AuthRoute.delete('/user', verifyToken, deleteUser);
+AuthRoute.delete('/user/:id', verifyToken, deleteUser);
