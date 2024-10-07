c4266750296f72ad2fa1048ecc07eb855aa93fb4
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-07 19:14:45 +0700

    Create: routes for getUser and getUser by id

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import verifyToken from '../middlewares/verifyToken';
-import { deleteUser, login, registerAdmin } from '../controllers/auth.controller';
+import { deleteUser, getUser, login, registerAdmin } from '../controllers/auth.controller';
 
 export const AuthRoute: Router = Router();
 
 AuthRoute.post('/login', login);
 AuthRoute.post('/register', verifyToken, registerAdmin);
+AuthRoute.get('/user', verifyToken, getUser);
+AuthRoute.get('/user/:uid', verifyToken, getUser);
 AuthRoute.delete('/user', verifyToken, deleteUser);
-AuthRoute.delete('/user/:id', verifyToken, deleteUser);
+AuthRoute.delete('/user/:uid', verifyToken, deleteUser);
