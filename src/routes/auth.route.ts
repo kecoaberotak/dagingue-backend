936410824013ec7f93844f201faa6ebdf96d3098
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-10 13:04:23 +0700

    Create: route for edit data user

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import verifyToken from '../middlewares/verifyToken';
-import { deleteUser, getUser, login, registerAdmin } from '../controllers/auth.controller';
+import { deleteUser, editUser, getUser, login, registerAdmin } from '../controllers/auth.controller';
 
 export const AuthRoute: Router = Router();
 
@@ -8,5 +8,6 @@ AuthRoute.post('/login', login);
 AuthRoute.post('/register', verifyToken, registerAdmin);
 AuthRoute.get('/user', verifyToken, getUser);
 AuthRoute.get('/user/:uid', verifyToken, getUser);
+AuthRoute.put('/user/:uid', verifyToken, editUser);
 AuthRoute.delete('/user', verifyToken, deleteUser);
 AuthRoute.delete('/user/:uid', verifyToken, deleteUser);
