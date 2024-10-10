e2cc6e42c18286d24f19e8089b251794464226a7
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-10 13:04:59 +0700

    Create: validation for update data user

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -19,6 +19,24 @@ export const registerValidation = (payload: RegisterTypes) => {
   return schema.validate(payload);
 };
 
+export const updateUserValidation = (payload: RegisterTypes) => {
+  const schema = Joi.object({
+    name: Joi.string().optional().messages({
+      'string.base': 'Nama tidak valid',
+    }),
+    email: Joi.string().email().optional().messages({
+      'string.base': 'email tidak valid',
+      'string.email': 'Format email tidak valid',
+    }),
+    password: Joi.string().min(6).optional().messages({
+      'string.base': 'password tidak valid',
+      'string.min': 'Password minimal harus 6 karakter',
+    }),
+  });
+
+  return schema.validate(payload);
+};
+
 export const loginValidation = (payload: LoginTypes) => {
   const schema = Joi.object({
     email: Joi.string().email().required().messages({
