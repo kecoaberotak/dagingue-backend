1ae4387a9e6cd0c514f0d5ceec17738b1a68d21d
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-12 12:40:30 +0700

    Add: env variable for email and app pass

--- src/config/environtment.ts
+++ src/config/environtment.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 
 const CONFIG = {
   port: process.env.PORT,
+  emailUser: process.env.EMAIL_USER,
+  emailPassword: process.env.EMAIL_PASS,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   apiKey: process.env.FIREBASE_API_KEY,
 };
