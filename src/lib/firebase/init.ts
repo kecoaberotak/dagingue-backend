7dc054183011d15291ce4298dc8e5436f7b1e482
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-14 16:08:06 +0700

    Edit: serviceAccount using variabel from env not json file anymore

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -1,6 +1,20 @@
+import 'dotenv/config';
 import admin from 'firebase-admin';
-const serviceAccount = require('../../../dagingue-dc5c9-firebase-adminsdk-6g8zo-4eb1a9ba3a.json');
-import CONFIG from '../../config/environtment';
+import CONFIG from '../../config/environment';
+
+const serviceAccount = {
+  type: process.env.FIREBASE_ADMIN_SDK_TYPE,
+  project_id: process.env.FIREBASE_ADMIN_SDK_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_SDK_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_SDK_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_ADMIN_SDK_AUTH_URI,
+  token_uri: process.env.FIREBASE_ADMIN_SDK_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_SDK_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_SDK_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_ADMIN_SDK_UNIVERSE_DOMAIN,
+} as admin.ServiceAccount;
 
 // Inisialisasi Firebase App
 export const firebase = admin.apps.length
