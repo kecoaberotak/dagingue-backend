77a41b1e3f4a368a965a667213b189b82180a6f7
Dennis Fernandes <dennisfernandes0212@gmail.com>
2024-10-14 14:35:59 +0700

    Edit: move firebase admin sdk to root

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-const serviceAccount = require('./dagingue-dc5c9-firebase-adminsdk-6g8zo-4eb1a9ba3a.json');
+const serviceAccount = require('../../../dagingue-dc5c9-firebase-adminsdk-6g8zo-4eb1a9ba3a.json');
 import CONFIG from '../../config/environtment';
 
 // Inisialisasi Firebase App
